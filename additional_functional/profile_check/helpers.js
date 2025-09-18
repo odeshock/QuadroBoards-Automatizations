894bcfb0205a795789d32c0dc04732042c29b16f
odeshock <vedcrt1004@gmail.com>
2025-09-19 00:26:47 +0400

    Update and rename helpers.js to helpers.js

--- additional_functional/profile_check/helpers.js
+++ additional_functional/profile_check/helpers.js
@@ -53,3 +53,17 @@ async function fetchCP1251Text(url, init){
   const buf = await res.arrayBuffer();
   return { res, text: new TextDecoder('windows-1251').decode(buf) };
 }
+function serializeFormCP1251(form){
+  const pairs = [];
+  for (const el of Array.from(form.elements||[])) {
+    if (!el.name || el.disabled) continue;
+    if ((el.type==='checkbox'||el.type==='radio') && !el.checked) continue;
+    if (el.tagName==='SELECT' && el.multiple) {
+      for (const opt of el.options) if (opt.selected)
+        pairs.push(encodeURIcp1251(el.name)+'='+encodeURIcp1251(opt.value));
+      continue;
+    }
+    pairs.push(encodeURIcp1251(el.name)+'='+encodeURIcp1251(el.value));
+  }
+  return pairs.join('&');
+}

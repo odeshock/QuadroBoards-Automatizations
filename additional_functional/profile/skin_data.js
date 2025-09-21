d436375b82b2b014baf1293ea8c88601c5f58072
odeshock <vedcrt1004@gmail.com>
2025-09-22 03:45:39 +0400

    Update skin_data.js

--- additional_functional/profile/skin_data.js
+++ additional_functional/profile/skin_data.js
@@ -1,7 +1,7 @@
 const icons_set = [
- `<img src="https://static.thenounproject.com/png/2185221-200.png>`,
- `<img src="https://cdn2.iconfinder.com/data/icons/harry-potter-colour-collection/60/07_-_Harry_Potter_-_Colour_-_Golden_Snitch-512.png>`,
- `<img src="https://i.pinimg.com/474x/c2/72/cb/c272cbe4f31c5a8d96f8b95256924e95.jpg>`,
+ `<img src="https://static.thenounproject.com/png/2185221-200.png">`,
+ `<img src="https://cdn2.iconfinder.com/data/icons/harry-potter-colour-collection/60/07_-_Harry_Potter_-_Colour_-_Golden_Snitch-512.png">`,
+ `<img src="https://i.pinimg.com/474x/c2/72/cb/c272cbe4f31c5a8d96f8b95256924e95.jpg">`,
 ];
 
 const plashka_set = [

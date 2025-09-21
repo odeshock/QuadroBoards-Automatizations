8a5896f9e6acecd14912f7220ced0057a9e2be3e
odeshock <vedcrt1004@gmail.com>
2025-09-22 03:28:04 +0400

    Update skin_data.js

--- additional_functional/profile/skin_data.js
+++ additional_functional/profile/skin_data.js
@@ -1,6 +1,6 @@
 const icons_set = [
  `https://static.thenounproject.com/png/2185221-200.png`,
- `https://cdn2.iconfinder.com/data/icons/harry-potter-colour-collection/60/07_-_Harry_Potter_-_Colour_-_Golden_Snitch-512.png  ,
+ `https://cdn2.iconfinder.com/data/icons/harry-potter-colour-collection/60/07_-_Harry_Potter_-_Colour_-_Golden_Snitch-512.png`,
  `https://i.pinimg.com/474x/c2/72/cb/c272cbe4f31c5a8d96f8b95256924e95.jpg`,
 ];
 

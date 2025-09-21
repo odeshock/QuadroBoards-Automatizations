62bdc1cbe0145d6ca4cbf10409589071e0ea34b4
odeshock <vedcrt1004@gmail.com>
2025-09-21 13:34:10 +0400

    Update button_update_total.js

--- additional_functional/chrono/button_update_total.js
+++ additional_functional/chrono/button_update_total.js
@@ -17,19 +17,6 @@
     ? window.CHRONO_CHECK.ForumInfo
     : [];
 
-  // === новые константы карт типов и статусов ===
-  const MAP_TYPE = window.CHRONO_CHECK?.EpisodeMapType || {
-    personal: ['personal', 'black'],
-    plot:     ['plot',     'black'],
-    au:       ['au',       'black']
-  };
-
-  const MAP_STAT = window.CHRONO_CHECK?.EpisodeMapStat || {
-    on:       ['active',   'green'],
-    off:      ['closed',   'teal'],
-    archived: ['archived', 'maroon']
-  };
-
   let busy = false;
 
   createForumButton({

4c63b44945b032d6a3e6f0071ddd18abd06a2d1c
odeshock <vedcrt1004@gmail.com>
2025-09-22 03:30:01 +0400

    Update skin_data.js

--- additional_functional/profile/skin_data.js
+++ additional_functional/profile/skin_data.js
@@ -6,10 +6,11 @@ const icons_set = [
 
 const plashka_set = [
   `<a class="modal-link">
-        <img src="https://upforme.ru/uploads/001c/14/5b/440/247944.gif" class="plashka">
-        <wrds>
-          я не подарок, но и ты не шаверма
-        </wrds></a>z`,`<a class="modal-link"><img src="https://upforme.ru/uploads/001c/14/5b/440/561829.gif" class="plashka"><wrds>twinkle twinkle little star</wrds></a>`
+   <img src="https://upforme.ru/uploads/001c/14/5b/440/247944.gif" class="plashka">
+   <wrds>я не подарок, но и ты не шаверма</wrds></a>`,
+  `<a class="modal-link">
+   <img src="https://upforme.ru/uploads/001c/14/5b/440/561829.gif" class="plashka">
+   <wrds>twinkle twinkle little star</wrds></a>`
 ];
 
 

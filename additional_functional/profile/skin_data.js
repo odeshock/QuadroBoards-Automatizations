503c3d28305c90b00bd402e5ca91bd80da5b2a90
odeshock <vedcrt1004@gmail.com>
2025-09-22 06:02:04 +0400

    Update skin_data.js

--- additional_functional/profile/skin_data.js
+++ additional_functional/profile/skin_data.js
@@ -1,4 +1,4 @@
-const icons_set = [
+const icon_set = [
  `<img class="icon" src="https://static.thenounproject.com/png/2185221-200.png">`,
  `<img class="icon" src="https://cdn2.iconfinder.com/data/icons/harry-potter-colour-collection/60/07_-_Harry_Potter_-_Colour_-_Golden_Snitch-512.png">`,
  `<img class="icon" src="https://i.pinimg.com/474x/c2/72/cb/c272cbe4f31c5a8d96f8b95256924e95.jpg">`,
@@ -13,12 +13,23 @@ const plashka_set = [
    <wrds>twinkle twinkle little star</wrds></a>`
 ];
 
+const icon_set = [
+ `<img class="back" src="https://upforme.ru/uploads/001c/14/5b/440/238270.gif">`,
+ `<img class="back" src="https://forumstatic.ru/files/001c/83/91/88621.png">`,
+];
 
-applyImagePicker(icons_set, '5',
+
+applyImagePicker(plashka_set, '3',
+  {
+    btnWidth: 229,    // ширина кнопки
+    btnHeight: 42, // высота кнопки
+    modalLinkMode: true, 
+  });
+applyImagePicker(icon_set, '5',
  {
    btnWidth: 44
 });
-applyImagePicker(plashka_set, '3',
+applyImagePicker(back_set, '3',
   {
     btnWidth: 229,    // ширина кнопки
     btnHeight: 42, // высота кнопки

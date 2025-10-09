7771a4b69a5615c57e1e3faa746bb4203bb61589
Anastasia <vedanastasia@MacBook-Air-Anastasia.local>
2025-10-09 19:00:20 +0400

    fix base_url

--- additional_functional/bank/config.js
+++ additional_functional/bank/config.js
@@ -2,7 +2,6 @@
 // config.js — Константы и конфигурации
 // ============================================================================
 
-//Основные url для работы
 export const ALLOWED_PARENTS = [
     "https://testfmvoice.rusff.me",   // тест
     "https://followmyvoice.rusff.me"        // прод
